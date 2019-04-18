e57f35ec3de763048b288ddae2f1595d94c567fd
Shafigh <bit_!088>
2019-04-18 12:00:09 -0400

    make first smart component to use data in store

---
+++ src/components/app.js
@@ -0,0 +1,13 @@
+import React from "react";
+import UserList from "../containers/user-list";
+
+export default function App() {
+  return (
+    <div>
+      <h2>Username List:</h2>
+      <UserList />
+      <hr />
+      <h2>User Details:</h2>
+    </div>
+  );
+}
---
+++ src/reducers/reducer_users.js
@@ -0,0 +1,28 @@
+export default function() {
+  return [
+    {
+      id: 1,
+      first: "Bucky",
+      last: "Roberts",
+      age: 71,
+      description: "Bucky is a react developer",
+      thumbnail: ""
+    },
+    {
+      id: 2,
+      first: "Shafigh",
+      last: "Parsa",
+      age: 31,
+      description: "Shafigh is a back-end developer",
+      thumbnail: ""
+    },
+    {
+      id: 3,
+      first: "Amin",
+      last: "Ameri",
+      age: 21,
+      description: "Amin is a front-end developer",
+      thumbnail: ""
+    }
+  ];
+}
